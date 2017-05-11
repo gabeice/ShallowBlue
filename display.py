333a5878913aaccec43e7c7aaa51f6577096d7cc
tacitblue <gabrielmcdonald1@gmail.com>
2017-05-11 16:43:25 -0400

    Game playable with display

--- display.py
+++ display.py
@@ -1,67 +1,95 @@
 import curses
+from board import Board
 
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-curses.start_color()
-stdscr.keypad(True)
-curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_GREEN)
-curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_RED)
+class Display(object):
+    def __init__(self, board):
+        self.screen = curses.initscr()
+        self.screen.keypad(True)
+        self.board = board
+        self.pos = [0,0]
+        self.selection = None
+        self.spaces = [[],[],[],[],[],[],[],[]]
+        self.textfield = curses.newwin(3, 8, 24, 0)
 
-pos = [0,0]
-key = None
-selection = [1,1]
-spaces = [[],[],[],[],[],[],[],[]]
+        curses.noecho()
+        curses.cbreak()
+        curses.start_color()
 
-for i in range(8):
-    for j in range(8):
-        win = curses.newwin(3, 6, i*3, j*6)
-        win.addstr(" ")
-        if (i+j)%2 == 0:
-            win.bkgd(curses.color_pair(1))
-        win.refresh()
-        spaces[i].append(win)
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_GREEN)
+        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_RED)
 
-textfield = curses.newwin(3, 8, 24, 0)
+    def print_message(self, message):
+        self.textfield.addstr(0, 0, message)
 
-spaces[0][0].bkgd(curses.color_pair(2))
-spaces[0][0].refresh()
+    def set_color(self, square, color):
+        self.spaces[square[0]][square[1]].bkgd(curses.color_pair(color))
 
-while key != 'q':
-    key = textfield.getkey()
-    if key in "ABCD":
-        if (pos == selection):
-            spaces[pos[0]][pos[1]].bkgd(curses.color_pair(3))
-        elif (pos[0] + pos[1])%2 == 0:
-            spaces[pos[0]][pos[1]].bkgd(curses.color_pair(1))
-        else:
-            spaces[pos[0]][pos[1]].bkgd(curses.color_pair(0))
-        spaces[pos[0]][pos[1]].refresh()
-        if key == "A":
-            pos[0] = (pos[0]-1)%8
-        elif key == "B":
-            pos[0] = (pos[0]+1)%8
-        elif key == "D":
-            pos[1] = (pos[1]-1)%8
-        else:
-            pos[1] = (pos[1]+1)%8
-        spaces[pos[0]][pos[1]].bkgd(curses.color_pair(2))
-        spaces[pos[0]][pos[1]].refresh()
+    def render(self):
+        for i in range(8):
+            for j in range(8):
+                win = curses.newwin(3, 6, i*3, j*6)
+                win.addstr(self.board.get([i,j]).letter)
+                if [i, j] == self.selection:
+                    win.bkgd(curses.color_pair(3))
+                elif (i+j)%2 == 0:
+                    win.bkgd(curses.color_pair(1))
+                win.refresh()
+                self.spaces[i].append(win)
+        # curses.napms(3000)
 
-    elif key == "\n":
-        if (selection[0] + selection[1])%2 == 0:
-            spaces[selection[0]][selection[1]].bkgd(curses.color_pair(1))
-        else:
-            spaces[selection[0]][selection[1]].bkgd(curses.color_pair(0))
-        spaces[selection[0]][selection[1]].refresh()
-        selection = pos[:]
-        spaces[selection[0]][selection[1]].bkgd(curses.color_pair(3))
-        spaces[selection[0]][selection[1]].refresh()
+    def get_move(self):
+        key = None
+        self.render()
+        if(self.selection):
+            self.set_color(self.selection, 3)
+        self.set_color(self.pos, 2)
+        self.spaces[self.pos[0]][self.pos[1]].refresh()
 
-    textfield.addstr(0, 0, str(pos))
+        while key != '\n':
+            key = self.textfield.getkey()
+            if key in "ABCD":
+                if (self.pos == self.selection):
+                    self.set_color(self.pos, 3)
+                elif (self.pos[0] + self.pos[1])%2 == 0:
+                    self.set_color(self.pos, 1)
+                else:
+                    self.set_color(self.pos, 0)
+                self.spaces[self.pos[0]][self.pos[1]].addstr(0, 0, self.board.get(self.pos).letter)
+                self.spaces[self.pos[0]][self.pos[1]].refresh()
 
-curses.nocbreak()
-stdscr.keypad(False)
-curses.echo()
-curses.endwin()
+                if key == "A":
+                    self.pos[0] = (self.pos[0]-1)%8
+                elif key == "B":
+                    self.pos[0] = (self.pos[0]+1)%8
+                elif key == "D":
+                    self.pos[1] = (self.pos[1]-1)%8
+                else:
+                    self.pos[1] = (self.pos[1]+1)%8
+                self.set_color(self.pos, 2)
+                self.spaces[self.pos[0]][self.pos[1]].addstr(0, 0, self.board.get(self.pos).letter)
+                self.spaces[self.pos[0]][self.pos[1]].refresh()
+
+        # if (selection[0] + selection[1])%2 == 0:
+        #     self.spaces[selection[0]][selection[1]].bkgd(curses.color_pair(1))
+        # else:
+        #     self.spaces[selection[0]][selection[1]].bkgd(curses.color_pair(0))
+        #     self.spaces[selection[0]][selection[1]].refresh()
+        #     selection = self.pos[:]
+        #     self.spaces[selection[0]][selection[1]].bkgd(curses.color_pair(3))
+        #     self.spaces[selection[0]][selection[1]].refresh()
+
+        return self.pos
+
+
+    def close(self):
+        self.screen.keypad(False)
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
+
+# test_board = Board()
+# test = Display(test_board)
+# selection = str(test.get_move())
+# test.close()
+# print (selection)
--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 from board import Board
+from display import Display
 import os
 
 class Game(object):
     def __init__(self):
         self.board = Board()
         self.turn = "white"
+        self.display = Display(self.board)
 
     def switch_turn(self):
         if self.turn == "white":
@@ -16,23 +18,24 @@ class Game(object):
         while not self.board.over():
             self.play_turn()
             self.switch_turn()
+        else:
+            self.display.close()
 
     def play_turn(self):
         from_pos = []
         while self.board.get(from_pos).color != self.turn:
-            os.system("clear")
-            self.board.render()
-            from_pos = input("%s's turn to move. Pick a square to move from: " % (self.turn))
-            from_pos = [int(num) for num in from_pos.split(",")]
+            from_pos = self.display.get_move()[:]
+        self.display.selection = from_pos
 
         to_pos = []
         while to_pos not in self.board.valid_moves(from_pos):
-            os.system("clear")
-            self.board.render()
-            to_pos = input("Pick a square to move to: ")
-            to_pos = [int(num) for num in to_pos.split(",")]
+            to_pos = self.display.get_move()[:]
 
         self.board.move_piece(from_pos, to_pos)
+        self.display.spaces[from_pos[0]][from_pos[1]].refresh()
+        self.display.spaces[to_pos[0]][to_pos[1]].refresh()
+        self.display.spaces[self.display.selection[0]][self.display.selection[1]].refresh()
+        self.display.selection = None
 
 test = Game()
 test.play()
