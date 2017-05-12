cec83bdd01420be8f6251bb35bedba71e007143e
tacitblue <gabrielmcdonald1@gmail.com>
2017-05-12 11:29:43 -0400

    Add error messages

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from board import Board
 from display import Display
 import os
+from pieces import opposite_color
 
 class Game(object):
     def __init__(self):
@@ -20,22 +21,28 @@ class Game(object):
             self.switch_turn()
         else:
             self.display.render()
-            self.display.print_message("%s wins! " % self.board.winner())
+            self.display.print_message("%s wins!   " % self.board.winner())
             self.display.textfield.refresh()
             self.display.sleep(2000)
             self.display.close()
 
     def play_turn(self):
-        self.display.print_message(" %s's turn" % self.turn)
+        self.display.print_message("  %s's turn   " % self.turn)
 
         from_pos = []
         while self.board.get(from_pos).color != self.turn:
             from_pos = self.display.get_move()[:]
+            if self.board.get(from_pos).color == opposite_color(self.turn):
+                self.display.print_message("Not your piece")
+                self.display.textfield.refresh()
         self.display.selection = from_pos
 
         to_pos = []
         while to_pos not in self.board.valid_moves(from_pos):
             to_pos = self.display.get_move()[:]
+            if to_pos not in self.board.valid_moves(from_pos):
+                self.display.print_message("Not a valid move")
+                self.display.textfield.refresh()
 
         self.board.move_piece(from_pos, to_pos)
         self.display.find(from_pos).refresh()
