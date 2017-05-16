05e48d519142bef5da6a4aa0e0f2b76f38d17a3e
tacitblue <gabrielmcdonald1@gmail.com>
2017-05-16 17:07:41 -0400

    Add method to check if a move is a mate move

--- ai_player.py
+++ ai_player.py
@@ -1,7 +1,8 @@
 from player import Player
 from pieces import opposite_color
-import random
 from board import Board
+import random
+import copy
 
 class AIPlayer(Player):
     def __init__(self, color):
@@ -16,6 +17,14 @@ class AIPlayer(Player):
                     result.append([i,j])
         return result
 
+    def opposition_pieces(self, board):
+        result = []
+        for i in range(8):
+            for j in range(8):
+                if board.get([i,j]).color == opposite_color(self.color):
+                    result.append([i,j])
+        return result
+
     def available_moves(self, board):
         moves = []
         positions = self.own_pieces(board)
@@ -24,6 +33,12 @@ class AIPlayer(Player):
             for move in possibilities:
                 moves.append([position, move])
         return moves
+
+    def mate_move(self, board, move):
+        test_board = Board()
+        test_board.board = copy.deepcopy(board)
+        test_board.move_piece(move[0], move[1])
+        return test_board.checkmate(opposite_color(self.color))
 
     def get_move(self, board, display):
         moves = self.available_moves(board)
